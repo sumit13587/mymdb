a9847d7a36ba57faf7802102ea9bfe1406b6cb9d
Sumit Sheth <sumit13587@gmail.com>
2020-10-17 10:20:45 +0100

    Initial

---
+++ mymdb-api/src/main/java/org/sumits/mymdbapi/MymdbApiApplication.java
@@ -0,0 +1,13 @@
+package org.sumits.mymdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymdbApiApplication.class, args);
+	}
+
+}
