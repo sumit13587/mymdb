063d4b328d6fccd70aa55970c978bdb5d3ee96ac
Sumit Sheth <sumit13587@gmail.com>
2020-10-18 17:52:57 +0100

    Make movie list sortable

--- mymdb-web/src/app/app.module.ts
+++ mymdb-web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie/movie-list/movie-list.component';
@@ -18,7 +19,8 @@ import { MovieListComponent } from './movie/movie-list/movie-list.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mymdb-web/src/app/movie/movie-list/movie-list.component.ts
+++ mymdb-web/src/app/movie/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MovieService } from '../movie.service'
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-movie-list',
@@ -12,11 +13,15 @@ export class MovieListComponent {
   movies: Array<any>;
   displayedColumns: string[] = ['movieId', 'title', 'status', 'popularity', 'runtime', 'voteAverage', 'voteCount'];
   dataSource;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private movieService: MovieService) {
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
     this.movieService.getAll().subscribe(data => {
       this.movies = data;
       this.dataSource = new MatTableDataSource(this.movies);
+      this.dataSource.sort = this.sort;
     });
   }
 
