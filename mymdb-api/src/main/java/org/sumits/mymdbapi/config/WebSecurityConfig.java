6bcb3c9136a2fde4262b00f0a80a823834196716
Sumit Sheth <sumit13587@gmail.com>
2020-10-18 14:19:44 +0100

    Add basic spring security

---
+++ mymdb-api/src/main/java/org/sumits/mymdbapi/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package org.sumits.mymdbapi.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .and()
+                .exceptionHandling()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/movies/list").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .logout();
+    }
+
+}
