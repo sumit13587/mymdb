5f9ecb280649022bc1bee245ba1de626322b7a51
Sumit Sheth <sumit13587@gmail.com>
2020-12-30 08:24:02 +0000

    Refactor angular modules

--- mymdb-web/src/app/app.module.ts
+++ mymdb-web/src/app/app.module.ts
@@ -1,28 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './movie/movie-list/movie-list.component';
-import { MovieDetailsComponent } from './movie/movie-details/movie-details.component';
+import { MovieModule } from './movie/movie.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent,
-    MovieDetailsComponent,
     PageNotFoundComponent
   ],
   imports: [
+    MovieModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
---
+++ mymdb-web/src/app/movie/movie.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MovieDetailsComponent } from "./movie-details/movie-details.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+    declarations: [MovieListComponent, MovieDetailsComponent],
+    imports: [
+        CommonModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+    exports: [MovieListComponent, MovieDetailsComponent]
+})
+
+export class MovieModule {}
