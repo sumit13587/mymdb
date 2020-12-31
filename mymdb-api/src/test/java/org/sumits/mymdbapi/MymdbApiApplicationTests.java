1c2a42f66cc6016e36a0d54373c2775f3b5bce4e
Sumit Sheth <sumit13587@gmail.com>
2020-12-31 07:22:33 +0000

    Add spring profiles, use 'local' profile with H2 database for tests

--- mymdb-api/src/test/java/org/sumits/mymdbapi/MymdbApiApplicationTests.java
+++ mymdb-api/src/test/java/org/sumits/mymdbapi/MymdbApiApplicationTests.java
@@ -2,8 +2,10 @@ package org.sumits.mymdbapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class MymdbApiApplicationTests {
 
 	@Test
